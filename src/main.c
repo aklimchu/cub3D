8bdf356eebcc81365e93775fbd9614f6744e0aa4
aklimchu <aklimchu@student.hive.fi>
2024-12-04 15:41:12 +0200

    commented out map parsing

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aklimchu <aklimchu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 09:10:41 by aklimchu          #+#    #+#             */
-/*   Updated: 2024/12/04 15:39:23 by aklimchu         ###   ########.fr       */
+/*   Updated: 2024/12/04 15:40:48 by aklimchu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,11 @@ int	main(int argc, char **argv)
 	t_cub	cub;
 	t_map	*map;
 
-	if (argc != 2)
+	/* if (argc != 2)
 		error_exit("Incorrect amount of arguments");
-	map = parse(argv[1]);
+	map = parse(argv[1]); */
+	(void)argc;
+	(void)argv;
 	create_image(&cub);
 	initialize_values(&cub);
 	mlx_loop_hook(cub.mlx, &draw_cub, &cub);
