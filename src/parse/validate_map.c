e436d58ebea19238e98b7e79b66d953bc87b63f5
Peter Leander <pleander@student.hive.fi>
2024-12-05 15:19:46 +0200

    Add check that start location is not outside of the map

--- src/parse/validate_map.c
+++ src/parse/validate_map.c
@@ -6,16 +6,16 @@
 /*   By: pleander <pleander@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 14:40:34 by pleander          #+#    #+#             */
-/*   Updated: 2024/12/05 14:54:03 by pleander         ###   ########.fr       */
+/*   Updated: 2024/12/05 15:19:13 by pleander         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-static int is_start_location(t_map_elems tok)
+static int	is_start_location(t_map_elems tok)
 {
 	if (tok == START_NO || tok == START_SO
-	 || tok == START_WE || tok == START_EA)
+		|| tok == START_WE || tok == START_EA)
 		return (1);
 	return (0);
 }
@@ -40,7 +40,27 @@ static void	find_player_start(t_map *map)
 			}
 			j++;
 		}
-		ft_printf("\n");
+		i++;
+	}
+}
+
+static void	check_player_inside_map(t_map *map)
+{
+	int		i;
+	int		j;
+	t_coord	s;
+
+	s = map->player_start;
+	i = map->player_start.x - 1;
+	while (i < map->player_start.x + 1)
+	{
+		j = map->player_start.y - 1;
+		while (j < map->player_start.y + 1)
+		{
+			if (map->map[i][j] == PADDING)
+				error_exit("Player start is outside the map");
+			j++;
+		}
 		i++;
 	}
 }
@@ -48,4 +68,5 @@ static void	find_player_start(t_map *map)
 void	check_map(t_map *map)
 {
 	find_player_start(map);
+	check_player_inside_map(map);
 }
