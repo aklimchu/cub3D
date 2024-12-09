f678fa29dd20b9282b39e117c12e8b378306ed16
Peter Leander <pleander@student.hive.fi>
2024-12-09 11:09:20 +0200

    Add some map parsing tests

---
+++ test/test.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    test.bash                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: pleander <pleander@student.hive.fi>        +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/12/09 10:47:12 by pleander          #+#    #+#              #
+#    Updated: 2024/12/09 10:54:22 by pleander         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+echo "Running CUB3D tests.."
+
+CUB3D="../cub3D"
+BROKEN_MAPS=$(ls broken_maps/*.cub)
+
+echo "Testing maps that should not work.."
+for MAP in $BROKEN_MAPS
+do
+	echo -n -e "\x1b[31m"
+	$CUB3D $MAP &> .tmp || (echo -n -e "\x1b[32m $MAP Failed successfully with error: \x1b[0m" && cat .tmp | tr '\n' ' ' && rm .tmp)
+	echo ""
+done
