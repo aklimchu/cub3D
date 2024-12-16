62bd522b506adae3fdc731632889b0d54068f9e2
aklimchu <aklimchu@student.hive.fi>
2024-12-16 13:13:15 +0200

    update_player formatted

--- src/graphics/update_player.c
+++ src/graphics/update_player.c
@@ -6,7 +6,7 @@
 /*   By: aklimchu <aklimchu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 11:19:18 by aklimchu          #+#    #+#             */
-/*   Updated: 2024/12/16 11:56:48 by aklimchu         ###   ########.fr       */
+/*   Updated: 2024/12/16 12:45:02 by aklimchu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,28 @@
 void	update_player(t_cub *cub)
 {
 	cub->player.dx = 0;
-   cub->player.dy = 0;
+	cub->player.dy = 0;
 
-	// Handle forward/backward movement
-    if (cub->keys.w) {
-        cub->player.dx += cos(cub->player.angle) * PLAYER_SPEED; // fps?
-        cub->player.dy += sin(cub->player.angle) * PLAYER_SPEED;
-    }
-    if (cub->keys.s) {
-        cub->player.dx -= cos(cub->player.angle) * PLAYER_SPEED;
-        cub->player.dy -= sin(cub->player.angle) * PLAYER_SPEED;
-    }
-
-    // Handle strafing (left/right movement)
-    if (cub->keys.a) {
-        cub->player.dx += sin(cub->player.angle) * PLAYER_SPEED;
-        cub->player.dy -= cos(cub->player.angle) * PLAYER_SPEED;
-    }
-    if (cub->keys.d) {
-        cub->player.dx -= sin(cub->player.angle) * PLAYER_SPEED;
-        cub->player.dy += cos(cub->player.angle) * PLAYER_SPEED;
-    }
-
+	if (cub->keys.w)
+	{
+		cub->player.dx += cos(cub->player.angle) * PLAYER_SPEED;
+		cub->player.dy += sin(cub->player.angle) * PLAYER_SPEED;
+	}
+	if (cub->keys.s)
+	{
+		cub->player.dx -= cos(cub->player.angle) * PLAYER_SPEED;
+		cub->player.dy -= sin(cub->player.angle) * PLAYER_SPEED;
+	}
+	if (cub->keys.a)
+	{
+		cub->player.dx += sin(cub->player.angle) * PLAYER_SPEED;
+		cub->player.dy -= cos(cub->player.angle) * PLAYER_SPEED;
+	}
+	if (cub->keys.d) {
+		cub->player.dx -= sin(cub->player.angle) * PLAYER_SPEED;
+		cub->player.dy += cos(cub->player.angle) * PLAYER_SPEED;
+	}
+	// sliding
 	if (check_next_tile(cub, cub->player.x + cub->player.dx, \
 		cub->player.y + cub->player.dy) == 1)
 	{
@@ -47,17 +47,16 @@ void	update_player(t_cub *cub)
 	{
 		check_angle(cub, false, &cub->player.dx, &cub->player.dy);
 	}
-
-    // Update position if the new tile is valid
-    if (check_next_tile(cub, cub->player.x + cub->player.dx, cub->player.y + cub->player.dy) == 0) {
-        cub->player.x += cub->player.dx;
-        cub->player.y += cub->player.dy;
-    }
-
-    // Handle rotation
-    if (cub->keys.left)
-        cub->player.angle -= ROTATION_SPEED;
-    if (cub->keys.right)
-        cub->player.angle += ROTATION_SPEED;
-    cub->player.angle = normalize_angle(cub->player.angle);
+	// Update position if the new tile is valid
+	if (check_next_tile(cub, cub->player.x + cub->player.dx, cub->player.y + cub->player.dy) == 0)
+	{
+		cub->player.x += cub->player.dx;
+		cub->player.y += cub->player.dy;
+	}
+	// Handle rotation
+	if (cub->keys.left)
+		cub->player.angle -= ROTATION_SPEED;
+	if (cub->keys.right)
+		cub->player.angle += ROTATION_SPEED;
+	cub->player.angle = normalize_angle(cub->player.angle);
 }
